f70e1d0daec06e177172b0144e991ff7119a4c03
Heorhii Siburov <g.siburov@gmail.com>
2021-05-05 00:13:17 -0700

    stop on pictures in menu for now...

--- src/components/MainBlock.js
+++ src/components/MainBlock.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Card, Image, Form, Input, Header } from 'semantic-ui-react';
+import MovieIcon from '../movieIcon.png';
 
 
 const MainBlock = () => {
@@ -14,16 +15,20 @@ const MainBlock = () => {
     }, [searchResults])
 
     useEffect(() => {
-        axios.get(`https://www.omdbapi.com/?s=${movie}&apikey=${process.env.REACT_APP_APIKEY}`)
-        .then(response => {
-            if (response.data.Response === "True") {
-                setSearchResults(response.data.Search)
-                // console.log("==>",response.data)
-            }
-        })
-        .catch(error => {
-            console.log(error)
-        })
+        if (movie) {
+            axios.get(`https://www.omdbapi.com/?s=${movie}&apikey=${process.env.REACT_APP_APIKEY}`)
+            .then(response => {
+                if (response.data.Response === "True") {
+                    setSearchResults(response.data.Search)
+                    // console.log("==>",response.data)
+                }
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        } else {
+            setSearchResults([]);
+        }
         setFetching(false);
     }, [movie])
 
@@ -41,25 +46,22 @@ const MainBlock = () => {
             <Header as='h4'>Movie title</Header>
             <Input loading={fetching} fluid icon='search' iconPosition='left' placeholder='Search...' value={movie} onChange={movieHandler}/>
         </Form>
-        <Card.Group id="Cards-group" itemsPerRow={5}>
 
-        {searchResults.map((movie) => (
-            // <div key={movie.imdbID}><p>{movie.Title}</p></div > 
-            // <Card key={movie.imdbID}>
-            //     <Image src={movie.Poster} wrapped ui={false} />
-            //     <Card.Content>
-            //     <Card.Header>{movie.Title}</Card.Header>
-            //     <Card.Meta>
-            //         <span className='date'>Joined in 2015</span>
-            //     </Card.Meta>
-            //     <Card.Description>
-            //         Matthew is a musician living in Nashville.
-            //     </Card.Description>
-            //     </Card.Content>
-            // </Card>
-                <Card key={movie.imdbID} raised image={movie.Poster} />
-                
-                ))}
+        <Card.Group centered={true} id="Cards-group" itemsPerRow={5}>
+        {searchResults.map((movie, i) => ( 
+            (movie.Poster === "N/A") ? 
+            <>
+                <Card key={i} raised>
+                    {/* <Card.Description>{movie.Title}</Card.Description> */}
+                    <Image src={MovieIcon} circular size='small' centered />
+                    {/* <Card.Description>{movie.Year}</Card.Description> */}
+                </Card>
+            </> 
+                : 
+            <>
+                <Card key={i} raised image={movie.Poster}/>
+            </>
+        ))}
         </Card.Group>
     </div>
     )
