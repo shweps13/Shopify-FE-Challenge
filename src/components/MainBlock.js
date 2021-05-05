55ffbde47243eacb990eb0ebde28eb747e74ffa9
Heorhii Siburov <g.siburov@gmail.com>
2021-05-04 21:22:54 -0700

    Added basic hooks

--- src/components/MainBlock.js
+++ src/components/MainBlock.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Form, Input, Header } from 'semantic-ui-react';
 
-const MainBlock = () => (
+
+const MainBlock = () => {
+
+    const [movie, setMovie] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+
+    useEffect(() => {
+        console.log(movie)
+    }, [movie])
+
+    const movieHandler = (e) => {
+        setMovie(e.target.value);
+    };
+
+    return(
     <div className="Main-form-block">
         <div className="Main-form-header">
             <Header as='h1'>The Shoppies</Header>
         </div>
         <Form id="Main-form-block">
-            <Input fluid icon='search' iconPosition='left' placeholder='Search...' />
+            <Header as='h4'>Movie title</Header>
+            <Input fluid icon='search' iconPosition='left' placeholder='Search...' value={movie} onChange={movieHandler}/>
         </Form>
     </div>
-)
+    )
+}
 
 export default MainBlock
