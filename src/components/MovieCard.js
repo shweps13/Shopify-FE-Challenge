14f830166a5bbd90d8e62dadc6c80900e99b820b
Heorhii Siburov <g.siburov@gmail.com>
2021-05-05 17:55:34 -0700

    Dimmer added

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Dimmer } from 'semantic-ui-react';
 
 const MovieCard = ({coverStyle, cover, title, titleStyle}) => {
 
+    const [dimmer, setDimmer] = useState({ active: false });
+
+    const handleShow = () => setDimmer({ active: true })
+    const handleHide = () => setDimmer({ active: false })
+
     return (
-        <div className="Card">
+        <Dimmer.Dimmable dimmed={dimmer.active} onMouseEnter={handleShow} onMouseLeave={handleHide}>
+
             <div className="Card-item">
                 <div className="Card-image">
                     <img id={coverStyle} src={cover} alt={title} />
                 </div>
                 <div className={titleStyle}>{title}</div>
             </div>
-        </div>
+            
+        <Dimmer active={dimmer.active} onClickOutside={handleHide} />
+        </Dimmer.Dimmable>
     )
 
 }
